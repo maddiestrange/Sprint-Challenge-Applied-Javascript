93912912f9a7ebf49409aa8b659e8abadfb0262f
maddiestrange <maddie.strange@gmail.com>
2019-06-21 11:19:30 -0500

    fixed dot typo on .active-tab , tabs work properly now

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -33,7 +33,7 @@ class TabLink {
     
     // Iterate through the NodeList removing the .active-tab class from each element
     tabs.forEach(function(item){
-      return item.classList.remove('.active-tab');
+      return item.classList.remove('active-tab');
     });
 
     // Select all of the elements with the .card class on them
